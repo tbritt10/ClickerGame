05cdd522de1fe91c1ff9cd746384ac6089da9f27
Tanner Britt <tannerbritt10@gmail.com>
2019-09-24 21:42:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/clickergame/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.clickergame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //Variable declarations
+    var walletBal: Int = 0
+    var dirtVal: Int = 1
+
+    //Click listener for Sell Dirt button
+    fun sellDirt(view: View) {
+        walletBal += dirtVal
+        walletView.text = walletBal.toString()
+    }
+}
