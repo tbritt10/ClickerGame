699b692aa1b634e70da5e0163c533762cb719d8f
Tanner Britt <tannerbritt10@gmail.com>
2019-09-25 10:38:59 -0500

    Creation of the pre-production branch

--- app/src/main/java/com/example/clickergame/MainActivity.kt
+++ app/src/main/java/com/example/clickergame/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
-
+//This is the pre-production branch
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
